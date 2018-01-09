29667f6e67dfbebef2267983a6597d9812fd5050
alain <developer2.reliefapps@gmail.com>
2018-01-09 11:54:14 +0100

    raise the upload max size limit to 400000M

--- Model/Limits.php
+++ Model/Limits.php
@@ -7,6 +7,6 @@ class Limits
     const limit_nb_roster_Members = 50; //Number of users in a Roster
     const limit_nb_roster_active_member = 10; //Number of rosters where a user is a member
     const limit_user_gallery_size = 52428800; //Size in bytes now 25 Mo
-    const max_document_size = "4M";
+    const max_document_size = "400000M";
 
 }
